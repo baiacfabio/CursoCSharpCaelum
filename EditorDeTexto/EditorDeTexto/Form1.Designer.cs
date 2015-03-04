867031672ef4bc449a163af8049272f415987571
Fabio Cezar Baía <baiacfabio@gmail.com>
2015-03-04 10:43:35 -0300

    Criando projeto de editor de texto.

---
+++ EditorDeTexto/EditorDeTexto/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace EditorDeTexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxConteudo = new System.Windows.Forms.TextBox();
+            this.buttonGravar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxConteudo
+            // 
+            this.textBoxConteudo.Location = new System.Drawing.Point(13, 13);
+            this.textBoxConteudo.Multiline = true;
+            this.textBoxConteudo.Name = "textBoxConteudo";
+            this.textBoxConteudo.Size = new System.Drawing.Size(259, 201);
+            this.textBoxConteudo.TabIndex = 0;
+            // 
+            // buttonGravar
+            // 
+            this.buttonGravar.Location = new System.Drawing.Point(13, 221);
+            this.buttonGravar.Name = "buttonGravar";
+            this.buttonGravar.Size = new System.Drawing.Size(75, 23);
+            this.buttonGravar.TabIndex = 1;
+            this.buttonGravar.Text = "Gravar";
+            this.buttonGravar.UseVisualStyleBackColor = true;
+            this.buttonGravar.Click += new System.EventHandler(this.buttonGravar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.buttonGravar);
+            this.Controls.Add(this.textBoxConteudo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxConteudo;
+        private System.Windows.Forms.Button buttonGravar;
+    }
+}
+
---
+++ EditorDeTexto/EditorDeTexto/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EditorDeTexto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("texto.txt"))
+            {
+                Stream entrada = File.Open("texto.txt", FileMode.Open);
+                StreamReader leitor = new StreamReader(entrada);
+                string linha = leitor.ReadLine();
+                while (linha != null)
+                {
+                    textBoxConteudo.Text += linha;
+                    linha = leitor.ReadLine();
+                }
+                leitor.Close();
+                entrada.Close();
+            }
+        }
+
+        private void buttonGravar_Click(object sender, EventArgs e)
+        {
+            Stream saida = File.Open("texto.txt", FileMode.Create);
+            StreamWriter escritor = new StreamWriter(saida);
+            escritor.Write(textBoxConteudo.Text);
+            escritor.Close();
+            saida.Close();
+        }
+    }
+}
