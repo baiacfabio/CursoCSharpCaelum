76174e99e9b32e18f5d88b68c433f67a8db1c4a8
Fabio Cezar Baía <baiacfabio@gmail.com>
2015-03-04 11:06:55 -0300

    Criando uma ConsoleApplication

---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mensagem que vai para o terminal");
+
+            TextReader leitor = Console.In;
+            TextWriter escritor = Console.Out;
+
+            string linha = leitor.ReadLine();
+            
+
+            while (linha != null)
+            {
+                linha = leitor.ReadLine();
+
+                //escreve o que o usuario digita
+                escritor.WriteLine("You say: "+linha);
+
+            }
+        }
+    }
+}
